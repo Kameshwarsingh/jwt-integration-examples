506c94358207120fd19d235d37140433f94981e3
Kameshwarsingh <Kameshwarsingh@users.noreply.github.com>
2019-10-21 12:44:42 -0400

    Update EntryController.java

--- client-web-application/src/main/java/com/sample/controller/EntryController.java
+++ client-web-application/src/main/java/com/sample/controller/EntryController.java
@@ -8,10 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EntryController {
 	 @RequestMapping("/")
     public String home(Model model) {
-    	model.addAttribute("domain", "dev-776359.okta.com!");
-    	model.addAttribute("clientId", "0oa1m2o3wch1iE7DJ357");
-    	model.addAttribute("redirectUrl", "http://localhost:8080/authorization-code/callback");
-    	//System.out.println("EntryController.home called");
         return "home";
     }
 }
